5ea3bf3df5b24828eff2a4ea658e033d918d99bd
Peter <peter@honixtech.com>
2015-11-04 21:49:55 +0800

    2015/11/04 First upload to WorldTraveler_CameraX.git

---
+++ src/com/kangyue/camerax/filters/NoneFilter.java
@@ -0,0 +1,9 @@
+package com.kangyue.camerax.filters;
+
+import org.opencv.core.Mat;
+
+public class NoneFilter implements Filter{
+	public void apply(final Mat src, final Mat dst) {
+		// Do nothing.
+		}
+}
